23a1fac33cd01c5a1d97ab8d371c0acffbeeea35
Narasinhareddy <narasinhareddy@Narasinhareddys-MacBook-Pro.local>
2017-06-05 19:32:33 +0530

    Testing gitRepo

--- app/src/main/java/com/example/narasinhareddy/lifecycledemo/MainActivity.java
+++ app/src/main/java/com/example/narasinhareddy/lifecycledemo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("lifecycle","onCreate invoked");
+        Log.d("lifecycl","onCreate invoked");
 
 
     }
