2c3450ff7d9ded6c758c020533a5ea5a2d7d7aa4
Narasinhareddy <snarasimha8261@gmail.com>
2017-11-05 13:50:06 +0530

    button added

--- app/src/main/java/com/example/narasinhareddy/lifecycledemo/Secon.java
+++ app/src/main/java/com/example/narasinhareddy/lifecycledemo/Secon.java
@@ -6,8 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Secon extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
