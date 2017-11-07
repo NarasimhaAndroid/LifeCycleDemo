78df8eef5927c3607abf7af98128c9f412919a26
Narasinhareddy <snarasimha8261@gmail.com>
2017-11-07 19:17:11 +0530

    editTextAdded

--- app/src/main/java/com/example/narasinhareddy/lifecycledemo/Secon.java
+++ app/src/main/java/com/example/narasinhareddy/lifecycledemo/Secon.java
@@ -7,16 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Secon extends AppCompatActivity {
 
     Button btn;
+    EditText etv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secon);
         btn= new Button(this);
+        etv=new EditText(this);
 
     }
 
